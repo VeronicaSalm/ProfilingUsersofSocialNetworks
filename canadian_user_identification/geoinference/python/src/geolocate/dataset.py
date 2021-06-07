27710cd1dd1b13c5b691d0c4c5460e81c06c3dc1
Veronica <vsalm@ualberta.ca>
2021-06-07 14:15:25 -0600

    Updated to python3 successfully, now cleaning up

--- canadian_user_identification/geoinference/python/src/geolocate/dataset.py
+++ canadian_user_identification/geoinference/python/src/geolocate/dataset.py
@@ -1,48 +1,20 @@
 ##
 #  Copyright (c) 2015, Derek Ruths, David Jurgens
+#  Modified by Veronica Salm, 2021
 #
 #  All rights reserved. See LICENSE file for details
 ##
 
 """
 A geoinference dataset is stored on disk in a directory with the following format:
-
     ds_root/
-        dataset.json - metadata about the dataset
-        posts.json.gz - all posts in the dataset in arbitrary order
-        users.json.gz - all posts in the dataset grouped by user
-
-This module provides a class for managing and accessing this directory as well
-as helper functions for building new datasets.
-
-**File formats**:
-
-All three core files in the dataset directory contain data in JSON format.
-
-  - `posts.json.gz` contains one post per line.  Each post is a JSON dictionary,
-    the contents of git@drgitlab.cs.mcgill.ca:druths/sysconfig.gitwhich is specific to the platform the post was taken from.
-
-  - `users.json.gz` contains one user per line.  Each user is a JSON dictionary
-    with at least the following keys:
-      - `user_id` is a string identifier for the user that flags them as unique
-        among all other users in the dataset.
-      - `posts` is a JSON list which contains all the posts belonging to that user.
-        These posts should have identical format and data to those in the
-        `posts.json.gz` file.  Moreover, every post present in the `posts.json` file
-        must be present in `users.json` and visa versa.
+        saved_graph.gt
 """
-# workaround for not having tkinter installed
-import matplotlib
-matplotlib.use("agg")
-
 import json
 import os, os.path
-import logging
 from graph_tool.all import *
 import gzip
 import sys
-
-logger = logging.getLogger(os.path.basename(__file__))
 
 class Dataset(object):
     """
@@ -103,7 +75,7 @@ class Dataset(object):
         G = zen.edgelist.read(self._mention_network_fname,directed=True,weighted=True)
 
         return G
-import sys
+
 def extract_user_mentions(obj):
     user_mentions = obj["entities"]["user_mentions"]
     mention_ids = []
@@ -137,23 +109,12 @@ def posts2dataset(dataset_dir,posts_dir,extract_user_id,extract_mentions,**kwarg
                 os.system("rm -r %s" % dataset_dir)
             else:
                 raise Exception('dataset directory %s exists' % dataset_dir)
-        logger.info('creating directory %s' % dataset_dir)
+        print('Creating directory %s' % dataset_dir)
         os.mkdir(dataset_dir)
 
-        # if the post_fname isn't in the dataset_dir,
-        # copy it there.
-    if os.path.dirname(posts_dir) != dataset_dir:
-        logger.info('copying posts dir %s' % posts_dir)
-        os.system('cp -r %s %s' % (posts_dir, dataset_dir))
-        posts_dir = os.path.join(dataset_dir, posts_dir)
-
-    # now make the users file
-    logger.info('building the users.json.gz file')
-    posts2users(posts_dir, extract_user_id, working_dir=dataset_dir)
-
     # now make the mention network
-    logger.info('building the mention network')
-        # NOTE: overwrote this to use my user mention function
+    print('Building the mention network...')
+    # TODO: update this function to work for follows
     posts2mention_network(posts_dir, extract_user_id,extract_user_mentions, working_dir=dataset_dir)
 
     # done!
@@ -180,7 +141,7 @@ def posts2mention_network(posts_dir,extract_user_id,
         working_dir = os.path.dirname(posts_dir)
 
     # bin the user data
-    logging.info('building the network')
+    print('Building the network...')
     cnt = 0
     # maps vertex descriptor --> user id
     G.vp.user_id = G.new_vertex_property("string")
@@ -197,8 +158,7 @@ def posts2mention_network(posts_dir,extract_user_id,
         for line in fh:
             cnt += 1
             post = json.loads(line)
-            uid = extract_user_id(post)
-            uid = str(uid)
+            uid = str(extract_user_id(post))
             mentions = extract_mentions(post)
 
             # vertex for this user
@@ -206,12 +166,8 @@ def posts2mention_network(posts_dir,extract_user_id,
                 v = G.add_vertex()
                 G.vp.user_id[v] = uid
                 vertices[uid] = v
-                # print("Added new vertex:", uid)
 
             for m in mentions:
-                #if G.has_edge(uid,m):
-                #        G.set_weight(uid,m,G.weight(uid,m)+1)
-                #else:
                 m = str(m)
                 if m not in vertices:
                     v = G.add_vertex()
@@ -225,24 +181,21 @@ def posts2mention_network(posts_dir,extract_user_id,
                 # fun fact: if you pass a string into G.iter_in_edges,
                 #           you get a segmentation fault :)
                 is_target_of_uid = [G.vp.user_id[edge[1]] for edge in list(G.iter_out_edges(uv))]
+                if uid == m and "retweeted_status" in post:
+                    # this is a retweet. Retweets automatically mention the retweeting
+                    # user, for some reason?
+                    continue
                 if m in is_target_of_uid:
                     # if m is already a target of this user, this is not the
                     # first time uid has mentioned m
                     # find the existing edge and simply increase its weight
                     e = edges[(uid, m)]
                     G.ep.weight[e] += 1
-                    # print(f"Incremented edge weight from {uid} to {m} to {G.ep.weight[e]}")
                 else:
                     # this is the first time uid has mentioned m
-                    #  if uid == m:
-                    #      if "retweeted_status" in post:
-                    #          # this is a retweet. Retweets automatically mention the retweeting
-                    #          # user, for some reason?
-                    #          continue
                     e = G.add_edge(uv,mv)
                     G.ep.weight[e] = 1
                     edges[(uid, m)] = e
-                    # print(f"Added edge from {uid} to {m}")
 
     print("User set:", len(user_set))
     print(cnt, "total tweets")
@@ -252,17 +205,17 @@ def posts2mention_network(posts_dir,extract_user_id,
     # if both (source, target) and (target, source) are edges,
     # then keep the edges
     # otherwise, remove them
-    #for source, target in list(edges.keys()):
-    #    if (target, source) not in edges or target == source:
-    #        G.remove_edge(edges[(source, target)])
-    #        del edges[(source, target)]
     for source, target in list(edges.keys()):
-        e = edges[(source, target)]
-        if G.ep.weight[e] < 2:
+      if (target, source) not in edges or target == source:
           G.remove_edge(edges[(source, target)])
           del edges[(source, target)]
-    #  print(f"Found {len(vertices)} vertices and {len(edges)} bidirectional edges.")
-    print(f"Found {len(vertices)} vertices and {len(edges)} edges with weight >= 2.")
+    #  for source, target in list(edges.keys()):
+    #      e = edges[(source, target)]
+    #      if G.ep.weight[e] < 2:
+    #        G.remove_edge(edges[(source, target)])
+    #        del edges[(source, target)]
+    print(f"Found {len(vertices)} vertices and {len(edges)} bidirectional edges.")
+    #  print(f"Found {len(vertices)} vertices and {len(edges)} edges with weight >= 2.")
 
 
     # Remove any vertices with degree 0 from the final graph
@@ -288,106 +241,3 @@ def posts2mention_network(posts_dir,extract_user_id,
 
     # done
     return
-
-
-def posts2users(posts_dir,extract_user_id,
-                working_dir=None,max_open_temp_files=256):
-    """
-    This method builds a valid `users.json.gz` file from the `posts.json.gz` file
-    specified.  Unless indicated otherwise, the directory containing the posts
-    file will be used as the working and output directory for the construction
-    process.
-
-    `extract_user_id` is a function that accepts a post and returns a string
-    user_id.
-    """
-
-    # figure out the working dir
-    if not working_dir:
-        working_dir = os.path.dirname(posts_dir)
-
-    # bin the user data
-    logger.info('binning user posts')
-
-    curr_temp_file_idx = -1
-
-    # A dict from a user-id to the file handle-id
-    user_assignments = {}
-    # A dict from the file handle-id to the actual file handle
-    file_handles = {}
-
-    # Sanity check methods for ensuring we're reading and writing
-    # all the data.
-    posts_seen = 0
-    user_posts_written = 0
-
-    for posts_fname in os.listdir(posts_dir):
-        fh = gzip.open(os.path.join(posts_dir, posts_fname),'r')
-        for line in fh:
-                post = json.loads(line)
-                uid = extract_user_id(post)
-                posts_seen += 1
-
-                if uid not in user_assignments:
-
-                        # Get the temp file this user should be in.
-                        # Assume that user-ids are randomly distribued
-                        # in some range such that the last three
-                        # digits of the id serve as a uniformly
-                        # distributed hash
-                        tmp_file_assignment = int(uid) % max_open_temp_files
-                        if not tmp_file_assignment in file_handles:
-                                # Write the temp file as gzipped files
-                                # because this splitting process gets
-                                # very expensive when processing large
-                                # datasets
-                                tmp_fname = os.path.join(working_dir,'tmp-%03d.json.gz'
-                                                         % tmp_file_assignment)
-                                logger.debug('creating temp file %s' % tmp_fname)
-
-                                tmp_fh = gzip.open(tmp_fname,'w')
-
-                                file_handles[tmp_file_assignment] = tmp_fh
-                        user_assignments[uid] = tmp_file_assignment
-
-                file_handles[user_assignments[uid]].write(line)
-
-        for idx,tmp_fh in file_handles.items():
-                tmp_fh.close()
-
-    # aggregate the users
-    logger.info('aggregating user data')
-
-    user_fh = gzip.open(os.path.join(working_dir,'users.json.gz'),'w')
-    for i in range(max_open_temp_files):
-        logging.debug('processing file %d' % i)
-
-        tmp_fname = os.path.join(working_dir,'tmp-%03d.json.gz' % i)
-        tmp_fh = gzip.open(tmp_fname,'r')
-
-        # aggregate data by tweets
-        user_posts = {}
-        for line in tmp_fh:
-            post = json.loads(line)
-            uid = extract_user_id(post)
-
-            if uid not in user_posts:
-                user_posts[uid] = []
-
-            user_posts[uid].append(post)
-
-        # write out the tweets by user
-        for uid,posts in user_posts.items():
-            post = "{}\n".format(json.dumps({'user_id':uid,'posts':posts}))
-            user_fh.write(bytes(post, 'utf-8'))
-            user_posts_written += len(posts)
-
-        # delete the temporary file
-        tmp_fh.close();
-        os.remove(tmp_fname)
-
-    # done
-    user_fh.close()
-    logger.debug("Read %s posts, wrote %s posts to users.json.gz"
-            % (posts_seen, user_posts_written))
-
