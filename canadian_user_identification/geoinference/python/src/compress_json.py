9f4bfbd8966fa02bad03ea674da6d9db1cfefc15
Veronica <vsalm@ualberta.ca>
2021-06-14 14:09:29 -0600

    removing old folders

--- canadian_user_identification/geoinference/python/src/compress_json.py
+++
@@ -1,9 +0,0 @@
-# Used to compress all tweet files, since the canadian tweets are given
-# in uncompressed jsonl format.
-# Assumes the tweets are in a folder called "canadians_april"
-import os
-
-for f in os.listdir("canadians_april"):
-    path = os.path.join("canadians_april", f)
-    os.system("gzip {}".format(path))
-
